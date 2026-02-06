b3422fba6b6a7dd8cd4328398ebe3622e3c2e8bb
mrtnsnchz <zekeryderproskater@gmail.com>
2026-02-05 22:19:21 -0700

    added privacy and cookies and removed existing cookies site

---
+++ wp-content/plugins/iubenda-cookie-law-solution/assets/js/cons.js
@@ -0,0 +1,1 @@
+var _iub=_iub||{};_iub.cons_instructions=_iub.cons_instructions||[],_iub.cons_instructions.push(["init",{api_key:iubConsParams.api_key,log_level:iubConsParams.log_level,logger:iubConsParams.logger,sendFromLocalStorageAtLoad:iubConsParams.send_from_local_storage_at_load},function(){}]);
